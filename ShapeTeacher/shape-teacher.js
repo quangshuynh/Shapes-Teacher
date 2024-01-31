923b27941dd4e6b03fc558ccf86c13767c0ccad6
quangshuynh <20378quang@gmail.com>
2024-01-31 03:18:55 +0000

    Initial commit

---
+++ ShapeTeacher/shape-teacher.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  $(".shapes").click(function() {
+    $(this).children().toggleClass("hide");
+  })
+
+
+})
---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $(".shapes").click(function() {
+    $(this).children().toggleClass("hide");
+  })
+})
